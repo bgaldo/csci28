d8daa010146e1c2cbcb4d3480ed2b4b304d50a48
Ben Galdo <beg867@g.harvard.edu>
2025-02-02 21:22:31 -0500

    Working version sans typescript

--- more03/more03.c
+++ more03/more03.c
@@ -4,30 +4,29 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include "termfuncs.h"
-//#define  PAGELEN        
+#include "termfuncs.h"     
 #define  ERROR          1
 #define  SUCCESS        0
 #define  has_more_data(x)   (!feof(x))
 #define CTL_DEV "/dev/tty"              /* source of control commands   */
 
-int  do_more(FILE *);
-int  pagelen[2];
-int  how_much_more(FILE *);
-void print_one_line(FILE *);
-void getPageSize();
+int  do_more(FILE *, int *pagelen);
+int  how_much_more(FILE *, int *pagelen);
+void print_one_line(FILE *, int *pagelen);
+void getPageSize(int *pagelen);
 
 int main( int ac , char *av[] )
 {
-        FILE    *fp;                    /* stream to view with more     */
+        int  pagelen[2];		/* val[0]=rows, val[1]=cols     */
+	FILE    *fp;                    /* stream to view with more     */
         int     result = SUCCESS;       /* return status from main      */
-	getPageSize();
+	getPageSize(pagelen);           /* get initial teminal size     */
         if ( ac == 1 )
-                result = do_more( stdin );
+                result = do_more( stdin, pagelen );
         else
                 while ( result == SUCCESS && --ac )
                         if ( (fp = fopen( *++av , "r" )) != NULL ) {
-                                result = do_more( fp ) ; 
+                                result = do_more( fp, pagelen ) ; 
                                 fclose( fp );
                         }
                         else
@@ -38,35 +37,49 @@ int main( int ac , char *av[] )
  *      args: FILE * opened to text to display
  *      rets: SUCCESS if ok, ERROR if not
  */
-int do_more( FILE *fp )
+int do_more( FILE *fp, int *pagelen )
 {
-        int     space_left = pagelen[0];        /* space left on screen */
-        int     reply;                          /* user request         */
-        FILE    *fp_tty;                        /* stream to keyboard   */
+        int     space_left = pagelen[0] - 1;     /* space left on screen */
+        int     reply;                           /* user request         */
+        FILE    *fp_tty;                         /* stream to keyboard   */
 
-        fp_tty = fopen( CTL_DEV, "r" );         /* connect to keyboard  */
-        while ( has_more_data( fp ) ) {         /* more input   */
-                if ( space_left <= 0 ) {                /* screen full? */
-                        reply = how_much_more(fp_tty);  /* ask user     */
-                        if ( reply == 0 )               /*    n: done   */
-                                break;
-                        space_left = reply;             /* reset count  */
+        fp_tty = fopen( CTL_DEV, "r" );          /* connect to keyboard  */
+        while ( has_more_data( fp ) ) {          /* more input   */
+                if ( space_left <= 0 ) {         /* screen full? */
+			printf("\033[7m more? \033[m"); /*reverse on a vt100 */
+                        reply = how_much_more(fp_tty, pagelen); /* ask user  */
+			fprintf(stdout, "\r                          \r");
+                        if ( reply == 0 )        /*    n: done    */
+                                exit(0);	 /* exit if user enters 'q'  */
+                        space_left = reply;      /* reset count   */
                 }
-                print_one_line( fp );
-                space_left--;                           /* count it     */
+		print_one_line( fp, pagelen );
+                space_left--;		         /* count it      */     
         }
-        fclose( fp_tty );                       /* disconnect keyboard  */
-        return SUCCESS;                         /* EOF => done          */
+        fclose( fp_tty );                       /* disconnect keyboard   */
+	pagelen[0] = 0;				/* set to 0 for next file*/
+        return SUCCESS;                         /* EOF => done           */
 }
 
 /*  print_one_line(fp) -- copy data from input to stdout until \n or EOF */
-void print_one_line( FILE *fp )
+void print_one_line( FILE *fp, int* pagelen)
 {
         int     c;
-
+	int 	charCount = 0; /* chars printed (doesn't account for tabs)*/
         while( ( c = getc(fp) ) != EOF && c != '\n' )
-                putchar( c ) ;
-        putchar('\n');
+	{
+		putchar(c);
+		if (++charCount == pagelen[1])  /* add check for line length */
+		{
+			putchar('\n');
+			return;
+		}
+	}
+	if (c == EOF) 				/* print prompt at EOF       */
+	{
+		printf("\033[7m more? - Next File\r \033[m");
+	}
+	else putchar( c );
 }
 
 /*  how_much_more -- ask user how much more to show
@@ -74,27 +87,26 @@ void print_one_line( FILE *fp )
  *      rets: number of additional lines to show: 0 => all done
  *      note: space => screenful, 'q' => quit, '\n' => one line
  */
-int how_much_more(FILE *fp)
+int how_much_more(FILE *fp, int *pagelen)
 {
         int     c;
-
-        printf("\033[7m more? \033[m");         /* reverse on a vt100   */
-        while( (c = getc(fp)) != EOF )          /* get user input       */
+	getPageSize(pagelen);  	                /* update pagelen          */
+        while( (c = rawgetc(fp)) != EOF )       /* get user input          */
         {
-                if ( c == 'q' )                 /* q -> N               */
+                if ( c == 'q' )                 /* q -> N                  */
                         return 0;
-                if ( c == ' ' )			/* ' ' => next page     */   
+                if ( c == ' ' )			/* ' ' => next page        */   
 		{
-			getPageSize();  	/* update pagelen       */
-			return pagelen[0];
+			return pagelen[0] - 1;  /* leave 1 line for prompt */
 		}                 
-                if ( c == '\n' )                /* Enter key => 1 line  */
+                if ( c == '\n' )                /* Enter key => 1 line     */
                         return 1;               
         }
         return 0;
 }
 
-void getPageSize() 
+/* Updates the current terminal size. Exits with -1 if get_term_size fails */
+void getPageSize(int *pagelen) 
 {
 	if(get_term_size(pagelen) == -1)
 	{
